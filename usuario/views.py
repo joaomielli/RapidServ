9c1ac631175048b85f3bd9ee90a6c24a9c50fd2a
Joao Mielli <mielli@alunos.utfpr.edu.br>
2023-09-22 09:28:53 -0300

    algumas correções

--- usuario/views.py
+++ usuario/views.py
@@ -4,8 +4,9 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as login_django
 from django.contrib.auth.decorators import login_required
-from .forms import ServicoForm 
+from .forms import ServicoForm
 from .models import Servico
+
 
 def cadastro(request):
     if request.method == "GET":
@@ -19,7 +20,6 @@ def cadastro(request):
 
         if usuario:
             return HttpResponse("Já existe um usuário com esse username")
-            print(f"usuario: {usuario}")
 
         usuario = User.objects.create_user(
             username=username, email=email, password=senha
@@ -46,24 +46,27 @@ def login(request):
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
+
 
 def criar_servico(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ServicoForm(request.POST)
         if form.is_valid():
             form.save()
-            return render('listar_servicos')
+            return render("listar_servicos")
     else:
         form = ServicoForm()
-    return render(request, 'criar_servico.html', {'form': form})
+    return render(request, "criar_servico.html", {"form": form})
+
 
 @login_required(login_url="/auth/login/")
 def listar_servicos(request):
     servicos = Servico.objects.all()
-    return render(request, 'listar_servicos.html', {'servicos': servicos})
+    return render(request, "listar_servicos.html", {"servicos": servicos})
+
 
 @login_required(login_url="/auth/login/")
 def perfil(request):
     usuario = request.user
-    return render(request, 'perfil.html', {'usuario': usuario})
+    return render(request, "perfil.html", {"usuario": usuario})
